dce0e80da6ed08de2d0e9c55c3efe106767ddc21
Pat Hartl <pat@pathar.tl>
2025-06-01 21:18:00 -0500

    Display names for sources and mute status

--- PJLink.Client/PJLinkCommands.cs
+++ PJLink.Client/PJLinkCommands.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace PJLink.Client;
 
 /// <summary>
@@ -60,14 +63,19 @@ public static class PJLinkCommands
         public enum Source
         {
             /// <summary>RGB input 1 (D-SUB)</summary>
+            [Display(Name = "VGA")]
             RGB_DSub = 11,
             /// <summary>RGB input 2 (5 BNC)</summary>
+            [Display(Name = "RGB BNC")]
             RGB_BNC = 12,
             /// <summary>RGB input 3 (DVI-I Analog)</summary>
+            [Display(Name = "DVI-I Analog")]
             RGB_DviAnalog = 13,
             /// <summary>RGB input 4 (SCART)</summary>
+            [Display(Name = "RGB SCART")]
             RGB_Scart = 14,
             /// <summary>RGB input 5 (M1-DA Analog)</summary>
+            [Display(Name = "M1-DA Analog")]
             RGB_M1DA = 15,
             /// <summary>RGB input 6</summary>
             RGB_6 = 16,
@@ -79,16 +87,22 @@ public static class PJLinkCommands
             RGB_9 = 19,
 
             /// <summary>Video input 1 (Composite RCA)</summary>
+            [Display(Name = "Composite")]
             Video_Composite = 21,
             /// <summary>Video input 2 (Component 3 RCA)</summary>
+            [Display(Name = "Component")]
             Video_Component = 22,
             /// <summary>Video input 3 (Component BNC)</summary>
+            [Display(Name = "Component BNC")]
             Video_ComponentBNC = 23,
             /// <summary>Video input 4 (S-Video)</summary>
+            [Display(Name = "S-Video")]
             Video_SVideo = 24,
             /// <summary>Video input 5 (D Terminal)</summary>
+            [Display(Name = "D Terminal")]
             Video_DTerminal = 25,
             /// <summary>Video input 6 (SCART)</summary>
+            [Display(Name = "SCART")]
             Video_Scart = 26,
             /// <summary>Video input 7</summary>
             Video_7 = 27,
@@ -98,31 +112,44 @@ public static class PJLinkCommands
             Video_9 = 29,
 
             /// <summary>Digital input 1 (DVI-I Digital)</summary>
+            [Display(Name = "DVI-I Digital")]
             Digital_DviDigital = 31,
             /// <summary>Digital input 2 (DVI-D)</summary>
+            [Display(Name = "DVI-D")]
             Digital_DviD = 32,
             /// <summary>Digital input 3 (HDMI)</summary>
+            [Display(Name = "HDMI")]
             Digital_Hdmi = 33,
             /// <summary>Digital input 4 (SDI)</summary>
+            [Display(Name = "SDI")]
             Digital_Sdi = 34,
             /// <summary>Digital input 5 (iLink/FireWire)</summary>
+            [Display(Name = "iLink/FireWire")]
             Digital_ILink = 35,
             /// <summary>Digital input 6 (M1-DA Digital)</summary>
+            [Display(Name = "M1-DA Digital")]
             Digital_M1DA = 36,
             /// <summary>Digital input 7 (M1-D)</summary>
+            [Display(Name = "M1-D")]
             Digital_M1D = 37,
             /// <summary>Digital input 8 (DisplayPort)</summary>
+            [Display(Name = "DisplayPort")]
             Digital_DisplayPort = 38,
             /// <summary>Digital input 9 (Wireless HDMI)</summary>
+            [Display(Name = "Wireless HDMI")]
             Digital_WirelessHdmi = 39,
 
             /// <summary>Storage input 1 (USB Type A)</summary>
+            [Display(Name = "USB Type A")]
             Storage_Usb = 41,
             /// <summary>Storage input 2 (PC Card Type II)</summary>
+            [Display(Name = "PC Card Type II")]
             Storage_PcCard = 42,
             /// <summary>Storage input 3 (CompactFlash)</summary>
+            [Display(Name = "CompactFlash")]
             Storage_CompactFlash = 43,
             /// <summary>Storage input 4 (SD Card)</summary>
+            [Display(Name = "SD Card")]
             Storage_SdCard = 44,
             /// <summary>Storage input 5</summary>
             Storage_5 = 45,
@@ -136,14 +163,19 @@ public static class PJLinkCommands
             Storage_9 = 49,
 
             /// <summary>Network input 1 (Wired LAN - RJ-45)</summary>
+            [Display(Name = "Wired LAN")]
             Network_Wired = 51,
             /// <summary>Network input 2 (Wireless LAN)</summary>
+            [Display(Name = "Wireless LAN")]
             Network_Wireless = 52,
             /// <summary>Network input 3 (USB Type B)</summary>
+            [Display(Name = "USB Type B")]
             Network_UsbB = 53,
             /// <summary>Network input 4 (Wireless USB)</summary>
+            [Display(Name = "Wireless USB")]
             Network_WirelessUsb = 54,
             /// <summary>Network input 5 (Bluetooth)</summary>
+            [Display(Name = "Bluetooth")]
             Network_Bluetooth = 55,
             /// <summary>Network input 6</summary>
             Network_6 = 56,
@@ -177,16 +209,22 @@ public static class PJLinkCommands
         public enum MuteState
         {
             /// <summary>Video mute off</summary>
+            [Display(Name = "Video Mute Off")]
             VideoMuteOff = 10,
             /// <summary>Video mute on</summary>
+            [Display(Name = "Video Mute On")]
             VideoMuteOn = 11,
             /// <summary>Audio mute off</summary>
+            [Display(Name = "Audio Mute Off")]
             AudioMuteOff = 20,
             /// <summary>Audio mute on</summary>
+            [Display(Name = "Audio Mute On")]
             AudioMuteOn = 21,
             /// <summary>Audio and video mute off</summary>
+            [Display(Name = "AV Mute Off")]
             AudioVideoMuteOff = 30,
             /// <summary>Audio and video mute on</summary>
+            [Display(Name = "AV Mute On")]
             AudioVideoMuteOn = 31
         }
     }
